89f74d4413c4900253fed7bf4aba1fc3bd142f52
Sami Tikkanen <800016346@omnia.fi>
2020-02-11 15:10:50 +0200

    updated version1, buttons not working yet

--- db_palvelin.php
+++ db_palvelin.php
@@ -17,17 +17,35 @@ echo "Tietokantayhteys luotu."; // tämä näyttää että yhteys on luotu web sivulla
 // aseta merkistökoodaus (muuten ääkköset sekoavat)
 $yhteys->set_charset("utf8");
 
-var_export($_POST);
+//var_export($_POST);
 
-/*$lisayssql = "INSERT INTO auto (rekisterinro, merkki, vari) VALUES ('FKC-560', 'Audi', 'punainen')";
+if(!isset($_REQUEST['button'])){
+	echo "Pääsy kielletty.";
+} //? error here
+if($_REQUEST['button'] == 'Tallenna'){
+	$rekisterinro = strtoupper($_POST['rekisterinro']);
+	$merkki = $_POST['merkki'];
+	$vari = $_POST['vari'];
 
-$tulos = $yhteys->query($lisayssql);
-
+	$lisayssql = "INSERT INTO auto (rekisterinro, merkki, vari) VALUES ('$rekisterinro', '$merkki', '$vari')";
+	$tulos = $yhteys->query($lisayssql);
 if ($tulos === TRUE) {
    echo "Auto lisätty.";
 } else {
-   echo "Virhe: " . $lisayssql . "<br>" . $conn->error;
+   echo "Virhe: " . $lisayssql . "<br>" . $yhteys->error;
 }
-*/
-
+}
+elseif ($_REQUEST['button'] == 'poista'){
+	$id = $_GET['id'];
+	$query = "DELETE FROM auto WHERE id = '$id'";
+	$tulos = $yhteys->query($lisayssql);
+if ($tulos === TRUE) {
+   echo "Auto poistettu.";
+} else {
+   echo "Virhe: " . $query . "<br>" . $yhteys->error;
+}
 ?>
+
+<form method="post" action="lisayslomake.php">
+<input type="submit" value="Takaisin">
+</form>
---
+++ lisayslomake.php
@@ -0,0 +1,81 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+	<meta name="viewport" content= "width=device-width, initial-scale=1.0"> 
+    <title>Lisäyslomake</title>
+
+	<style>
+	label {
+		display: inline-block;
+		width: 150px;
+	}
+
+	input[type="submit"] {
+		margin-left: 153px;
+		margin-top: 5px;
+	}
+
+	span {
+		display: inline-block;
+		width: 120px;
+		border: 1px solid grey;
+	}
+
+	ul {
+		list-style-type: none;
+	}
+
+	a {
+		padding: 2px;
+	}
+
+	a:hover {
+		color: red;
+		background-color: pink;
+	}
+</style>
+</head>
+<body>
+	<div> UUSI REKISTERINUMERO TIETOKANTAAN</div><br>
+	<form action="db_palvelin.php" method="post">
+		<label>Rekisterinumero</label>
+		<input name="rekisterinro" placeholder="Rekisterinumero"><br>
+		<label>Merkki</label>
+		<input name="merkki" placeholder="Merkki"><br>
+		<label>Väri</label>
+		<input name="vari" placeholder="Väri"><br>
+
+		<input type="submit" name="button" value='Tallenna'>
+	</form>
+	<?php
+	$palvelin = "localhost";
+	$kayttaja = "Matti";
+	$salasana = "Matti1";
+	$tietokanta = "db";
+
+	$yhteys = new mysqli($palvelin, $kayttaja, $salasana, $tietokanta);
+
+	if ($yhteys->connect_error) {
+		die("Yhteyden muodostaminen epäonnistui: " .$yhteys->connect_error);
+	}
+
+	$query = "SELECT id,rekisterinro,merkki,vari FROM auto ORDER BY rekisterinro LIMIT 25";
+	$tulos = $yhteys->query($query);
+
+	if (!$tulos) {
+		echo "Virhe: $query<br>".$yhteys->error;
+	}
+	echo "<ul>";
+	while($rivi = $tulos->fetch_assoc()) {
+		$id = $rivi['id'];
+		$rekisterinro = $rivi["rekisterinro"];
+		$merkki = $rivi["merkki"];
+		$vari = $rivi["vari"];
+		echo "<li><span>$rekisterinro</span><span>$merkki</span><span>$vari</span><a href=\"db_palvelin.php?id=$id&button=poista\">Poista</a><a href=\"db_palvelin.php?id=$id&button=muuta\">Muuta</a></li>";
+	}
+	echo "</ul>";
+	?>
+</body>
+</html>
