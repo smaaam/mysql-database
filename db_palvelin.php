260507451dabc7516095917ff89fb24bd7b616c5
Sami <tikkanen12@gmail.com>
2020-02-11 20:51:53 +0200

    updated v2

--- db_palvelin.php
+++ db_palvelin.php
@@ -22,13 +22,14 @@ $yhteys->set_charset("utf8");
 if(!isset($_REQUEST['button'])){
 	echo "Pääsy kielletty.";
 } //? error here
-if($_REQUEST['button'] == 'Tallenna'){
-	$rekisterinro = strtoupper($_POST['rekisterinro']);
-	$merkki = $_POST['merkki'];
-	$vari = $_POST['vari'];
-
-	$lisayssql = "INSERT INTO auto (rekisterinro, merkki, vari) VALUES ('$rekisterinro', '$merkki', '$vari')";
-	$tulos = $yhteys->query($lisayssql);
+if ($_REQUEST['button'] == 'tallenna'){  
+    
+$rekisterinro = strtoupper($_POST['rekisterinro']);
+$merkki= $_POST['merkki'];
+$vari = $_POST['vari'];
+    
+$lisayssql = "INSERT INTO auto (rekisterinro, merkki, vari) VALUES ('$rekisterinro', '$merkki', '$vari')";
+$tulos = $yhteys->query($lisayssql);
 if ($tulos === TRUE) {
    echo "Auto lisätty.";
 } else {
@@ -36,14 +37,16 @@ if ($tulos === TRUE) {
 }
 }
 elseif ($_REQUEST['button'] == 'poista'){
-	$id = $_GET['id'];
-	$query = "DELETE FROM auto WHERE id = '$id'";
-	$tulos = $yhteys->query($lisayssql);
-if ($tulos === TRUE) {
-   echo "Auto poistettu.";
-} else {
-   echo "Virhe: " . $query . "<br>" . $yhteys->error;
-}
+    $id = $_GET['id'];    
+    $query = "DELETE FROM auto WHERE id = '$id'";
+    $tulos = $yhteys->query($query);
+    if ($tulos === TRUE) {
+      echo "Auto poistettu.";
+      } 
+    else {
+      echo "Virhe: " . $query . "<br>" . $yhteys->error;
+      }
+    }
 ?>
 
 <form method="post" action="lisayslomake.php">
